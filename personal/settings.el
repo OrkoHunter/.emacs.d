29464314f78d030abafe9c0c4869e78d06739ed0
Himanshu Mishra <himanshu@orkohunter.net>
2019-03-02 20:06:15 +0530

    Enable backups in a different directory

--- personal/settings.el
+++ personal/settings.el
@@ -1,1 +1,9 @@
-(setq projectile-project-search-path '("~/projects/"))
+(setq projectile-project-search-path '("~/workspace/"))
+
+; More and more backups of the files. Move them to different location as well
+(setq backup-directory-alist `(("." . "~/.emacs.d/backup")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
